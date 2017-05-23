1805ee88c33307378510fb9184a6d5a7e4d9d72d
Alexandre BODIN <bodin.alex@gmail.com>
2017-05-23 23:23:11 +0200

    Handles context to render after decorator are applied

--- src/index.js
+++ src/index.js
@@ -2,53 +2,61 @@ import React from 'react'
 import addons from '@kadira/storybook-addons'
 import reactElementToJSXString from 'react-element-to-jsx-string'
 
+const renderJsx = (code, options) => {
+  for (let i = 0; i < options.skip; i++) {
+    if (typeof code === 'undefined') {
+      console.warn('Cannot skip undefined element')
+      return
+    }
+
+    if (React.Children.count(code) > 1) {
+      console.warn('Trying to skip an array of elements')
+      return
+    }
+
+    if (typeof code.props.children === 'undefined') {
+      console.warn('Not enough children to skip elements.')
+
+      if (typeof code.type === 'function' && code.type.name === '') code = code.type(code.props)
+    } else {
+      if (typeof code.props.children === 'function') {
+        code = code.props.children()
+      } else {
+        code = code.props.children
+      }
+    }
+  }
+
+  if (typeof code === 'undefined') return console.warn('Too many skip or undefined component')
+
+  while (typeof code.type === 'function' && code.type.name === '')
+    code = code.type(code.props)
+
+  const ooo = typeof options.displayName === 'string'
+    ? Object.assign({}, options, { displayName: () => options.displayName })
+    : options
+
+  return React.Children.map(code, c => reactElementToJSXString(c, ooo)).join('\n')
+}
+
 export default {
-  addWithJSX(kind, story, opts = {}) {
+  addWithJSX(kind, storyFn, opts = {}) {
     const defaultOpts = {
       skip: 0,
       showFunctions: true,
     }
-    const channel = addons.getChannel()
-    const result = this.add(kind, story)
     const options = Object.assign({}, defaultOpts, opts)
-    let code = story()
+    const channel = addons.getChannel()
 
-    for (let i = 0; i < options.skip; i++) {
-      if (typeof code === 'undefined') {
-        console.warn('Cannot skip undefined element')
-        return
-      }
+    const result = this.add(kind, context => {
+      const story = storyFn(context)
 
-      if (React.Children.count(code) > 1) {
-        console.warn('Trying to skip an array of elements')
-        return
-      }
+      const jsx = renderJsx(story, options)
+      channel.emit('kadira/jsx/add_jsx', result.kind, kind, jsx)
 
-      if (typeof code.props.children === 'undefined') {
-        console.warn('Not enough children to skip elements.')
+      return story
+    })
 
-        if (typeof code.type === 'function' && code.type.name === '') code = code.type(code.props)
-      } else {
-        if (typeof code.props.children === 'function') {
-          code = code.props.children()
-        } else {
-          code = code.props.children
-        }
-      }
-    }
-
-    if (typeof code === 'undefined') return console.warn('Too many skip or undefined component')
-
-    while (typeof code.type === 'function' && code.type.name === '')
-      code = code.type(code.props)
-
-    const ooo = typeof options.displayName === 'string'
-      ? Object.assign({}, options, { displayName: () => options.displayName })
-      : options
-
-    const compiledCode = React.Children.map(code, c => reactElementToJSXString(c, ooo)).join('\n')
-
-    channel.emit('kadira/jsx/add_jsx', result.kind, kind, compiledCode)
     return result
   },
 }
