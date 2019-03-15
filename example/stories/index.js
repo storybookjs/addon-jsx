64e0d363f38e08649759e9d4c82ea448796f2cc4
Andrew Lisowski <lisowski54@gmail.com>
2019-03-15 16:31:31 -0700

    fix tests

--- example/stories/index.js
+++ example/stories/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { setAddon } from '@storybook/react'
-import JSXAddon from '../../lib/index'
+import React from 'react';
+import { setAddon } from '@storybook/react';
+import { JSXAddon } from '../../lib/index';
 
-import SimpleStories from './simple'
-import DeepStories from './deep'
-import FunctionStories from './functions'
-import ArrayStories from './array'
-import WithPropsStories from './withProps'
+import SimpleStories from './simple';
+import DeepStories from './deep';
+import FunctionStories from './functions';
+import ArrayStories from './array';
+import WithPropsStories from './withProps';
 
-setAddon(JSXAddon)
+setAddon(JSXAddon);
 
-SimpleStories()
-DeepStories()
-FunctionStories()
-ArrayStories()
-WithPropsStories()
+SimpleStories();
+DeepStories();
+FunctionStories();
+ArrayStories();
+WithPropsStories();
